d4beba696abb01ba93a58ae566283636fbc4b57f
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-11-23 07:37:10 +0900

    3章まで完了

---
+++ shain_factory.rb
@@ -0,0 +1,21 @@
+require_relative 'shain'
+
+class ShainFactory
+  def create(type, kihonkyu)
+    eval "#{type}.new(#{kihonkyu})"
+  end
+
+  # def create(type, kihonkyu)
+  #   shain = nil
+  #   if type == 'Tanto'
+  #     shain = Tanto.new(kihonkyu)
+  #   elsif type == 'Shunin'
+  #     shain = Shunin.new(kihonkyu)
+  #   elsif type == 'Bucho'
+  #     shain = Bucho.new(kihonkyu)
+  #   elsif type == 'Torishimariyaku'
+  #     shain = Torishimariyaku.new(kihonkyu)
+  #   end
+  #   shain
+  # end
+end
